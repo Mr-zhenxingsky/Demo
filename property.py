848f96ccc70b890f57639b002232ee1b5ee86fc7
zhenxing <1597561730@qq.com>
2019-08-19 10:29:27 +0800

    propertyDemo

---
+++ property.py
@@ -0,0 +1,34 @@
+class Account(object):
+    """
+    account class
+    amount is $.
+    """
+    def __init__(self,rate):
+        self.__amt = 0
+        self.rate = rate
+
+    @property
+    def amount(self):
+        """ account balance($)"""
+        return self.__amt
+
+    @property
+    def cny(self):
+        """ account balance(yuan)"""
+        return self.__amt * self.rate
+
+    @amount.setter
+    def amount(self,value):
+        if value < 0:
+            print("Sorry,no negative amount in the account.")
+            return
+        self.__amt = value
+
+if __name__ == '__main__':
+    acc = Account(rate=6.6)
+    acc.amount = 20
+    print("Dollar ammount:",acc.amount)
+    print("In CNY:",acc.cny)
+    acc.amount = -100
+    print("Dollar amount:",acc.amount)
+
